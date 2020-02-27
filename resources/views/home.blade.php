abd5c33aa0ce4da7443a5d7b013aba000234f91e
Daniel <dk377@hotmail.com>
2020-02-27 22:32:50 +0000

    dashboard atualizada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,44 @@
 @extends('layouts.master')
 
 @section('content')
-<page tamanho="12" >
 
 
-	
+    <h4>Resumo do mês</h4>
+
+    <b>Quantidade de vendas</b>: {{ $data['total'] }}
+    <br>
+    <b>Quantidade de pedidos pago</b>: {{ $data['total_paid'] }}
+    <br>
+    <b>Total de pedidos pago</b>: R${{ sprintf(number_format($data['value_paid'], 2, ',','.')) }}
+    <br>
+    <b>Quantidade de pedidos a receber</b>: {{ $data['total_no_paid'] }}
+    <br>
+    <b>Total de pedidos a receber</b>: R${{ sprintf(number_format($data['value_no_paid'], 2, ',','.')) }}
+
+    <br>
+    <hr>
 
 
+    <h4>Entregas do dia</h4>
+    @if($deliveries->count())
+        <ul>
+            @foreach($deliveries as $delivery)
+                <li>
+                    {{ $delivery->product }} - {{ $delivery->description ?: 'Sem descrição' }} - R${{ $delivery->price }}
+                </li>
+            @endforeach
+        </ul>
 
-</page>
+        <br>
+        Total: {{ $data['value_day'] }}
+    @else
+        <div class="alert alert-primary">
+            Não há nenhuma entrega para hoje!
+        </div>
+    @endif
 
+@endsection
 
+@section('scripts')
 
 @endsection
