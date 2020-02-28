7a97feede1f2e5857ad4682cb8d3e0954299223d
Daniel <dk377@hotmail.com>
2020-02-28 17:02:14 +0000

    ajustes em algumas telas

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -49,9 +49,9 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href={{route('home')}} class="brand-link">
         <img src="" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel') }}</span>
+        <span class="brand-text font-weight-light">Scorp</span>
       </a>
 
       <!-- Sidebar -->
@@ -69,6 +69,16 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+              <li class="nav-item">
+                  <a href="{{route('home')}}" class="nav-link">
+                      <i class="nav-icon  fas fa-home"></i>
+
+                      <p>
+                          Home
+
+                      </p>
+                  </a>
+              </li>
 
             <li class="nav-item">
               <a href="{{route('customers.index')}}" class="nav-link">
@@ -83,25 +93,25 @@
                 <a href="{{route('seams.index')}}" class="nav-link">
                   <i class="nav-icon fas fa-cut"></i>
                   <p>
-                    Costuras
+                    Pedidos de Confecção
 
                   </p>
                 </a>
               </li>
-              <li class="nav-item">
-              <a href="{{route('balances.index')}}" class="nav-link">
-                  <i class="nav-icon fas fa-dollar-sign"></i>
-                  <p>
-                    Pagamento
+{{--              <li class="nav-item">--}}
+{{--              <a href="{{route('balances.index')}}" class="nav-link">--}}
+{{--                  <i class="nav-icon fas fa-dollar-sign"></i>--}}
+{{--                  <p>--}}
+{{--                    Pagamento--}}
 
-                  </p>
-                </a>
-              </li>
+{{--                  </p>--}}
+{{--                </a>--}}
+{{--              </li>--}}
               <li class="nav-item">
               <a href="{{ route('sales') }}" class="nav-link">
                 <i class="nav-icon fas fa-file-invoice-dollar"></i>
                   <p>
-                    Relatório de Pagamentos
+                       Pagamentos e Relatório
                   </p>
                 </a>
               </li>
